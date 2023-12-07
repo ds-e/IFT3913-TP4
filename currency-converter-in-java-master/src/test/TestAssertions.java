7053d6d2ad5f00880ffe9bb0a6c0e0766d1a65c6
Zeinab-Taleb <145884839+Zeinab-Taleb@users.noreply.github.com>
2023-12-06 21:19:06 -0500

    Update TestAssertions.java

--- currency-converter-in-java-master/src/test/TestAssertions.java
+++ currency-converter-in-java-master/src/test/TestAssertions.java
@@ -2,18 +2,14 @@ package test;
 
 import static org.junit.Assert.*;
 
-
-import static org.junit.Assert.assertEquals;
-
 import java.util.ArrayList;
 
-import org.junit.Rule;
+
 import org.junit.Test;
 
 import currencyConverter.Currency;
-import currencyConverter.MainWindow;
-import org.junit.rules.ExpectedException;
 
+import currencyConverter.MainWindow;
 public class TestAssertions  {
 	  ArrayList<Currency> currencies = Currency.init();
 	 @Test
@@ -22,15 +18,15 @@ public class TestAssertions  {
 	        currencies.add(new Currency("Canadian Dollar", "CAD")); 
 	        currencies.add(new Currency("Australian Dollar", "AUD"));
 	        
-	        Currency currency1 = new Currency("US Dollar", "USD");
-	        Currency currency2 =new Currency("Euro", "EUR") ;
-	        Currency currency3 =new Currency("British Pound", "GBP") ;
-	        Currency currency4 =new Currency("Swiss Franc", "CHF") ;
-	        Currency currency7 = new Currency("Chinese Yuan Renminbi", "CNY") ;
-		    Currency currency8= new Currency("Japanese Yen", "JPY") ;
-	        Currency currency5 =new Currency("Canadian Dollar", "CAD") ;
-	        Currency currency6 =new Currency("Australian Dollar ", "AUD") ;
-	     
+//	        Currency currency1 = new Currency("US Dollar", "USD");
+//	        Currency currency2 =new Currency("Euro", "EUR") ;
+//	        Currency currency3 =new Currency("British Pound", "GBP") ;
+//	        Currency currency4 =new Currency("Swiss Franc", "CHF") ;
+//	        Currency currency7 = new Currency("Chinese Yuan Renminbi", "CNY") ;
+//		    Currency currency8= new Currency("Japanese Yen", "JPY") ;
+//	        Currency currency5 =new Currency("Canadian Dollar", "CAD") ;
+//	        Currency currency6 =new Currency("Australian Dollar ", "AUD") ;
+//	     
 	        assertTrue(currencies.get(0).getName()=="US Dollar");
 	        assertTrue(currencies.get(1).getName()=="Euro");
 	        assertTrue(currencies.get(2).getName()=="British Pound");
@@ -39,79 +35,51 @@ public class TestAssertions  {
 	        assertTrue(currencies.get(5).getName()=="Japanese Yen");
 	        assertTrue(currencies.get(6).getName()=="Canadian Dollar");
 	        assertTrue(currencies.get(7).getName()=="Australian Dollar");
-	      
+	      	     
+	        currencies.get(6).setExchangeValues("USD", 0.74);
+	        currencies.get(6).setExchangeValues("GBP", 0.58);
+	        currencies.get(6).setExchangeValues("CHF", 0.64);
+	        currencies.get(6).setExchangeValues("CNY", 5.26);
+	        currencies.get(6).setExchangeValues("JPY", 108.26);
+	        currencies.get(6).setExchangeValues("AUD", 1.12);
+	        currencies.get(6).setExchangeValues("EUR", 0.68);
+	        currencies.get(7).setExchangeValues("USD", 0.66);
+	        currencies.get(7).setExchangeValues("GBP", 0.52);
+	        currencies.get(7).setExchangeValues("CHF", 0.57);
+	        currencies.get(7).setExchangeValues("CNY", 4.65);
+	        currencies.get(7).setExchangeValues("JPY", 96.43);
+	        currencies.get(7).setExchangeValues("CAD", 0.89);
+	        currencies.get(0).setExchangeValues("CAN", 1.36);
+	        currencies.get(0).setExchangeValues("AUD", 1.53);
+	        currencies.get(2).setExchangeValues("CAN", 1.71);
+	        currencies.get(2).setExchangeValues("AUD", 1.92); 
+	        currencies.get(1).setExchangeValues("CAN", 1.47);
+	        currencies.get(1).setExchangeValues("AUD", 1.65); 
+	        currencies.get(3).setExchangeValues("CAN", 1.55);
+	        currencies.get(3).setExchangeValues("AUD", 1.75); 
+	     
+	        currencies.get(6).defaultValues();
+	        currencies.get(7).defaultValues();
+   
+	        Double convertedAmount =MainWindow.convert("Canadian Dollar","US Dollar", currencies,198.0);   
+	        assertEquals(146.52, convertedAmount, 0.01);
+	        convertedAmount = MainWindow.convert("Canadian Dollar","British Pound", currencies,198.0);
+	        assertEquals(114.84, convertedAmount, 0.01);
+	        convertedAmount = MainWindow.convert("Canadian Dollar", "Euro",currencies , 198.0);
+	        assertEquals(134.64, convertedAmount, 0.01);
+	        convertedAmount =  MainWindow.convert("Canadian Dollar", "Swiss Franc", currencies , 198.0);
+	        assertEquals(126.72, convertedAmount, 0.01);
+	        convertedAmount =MainWindow.convert("Canadian Dollar", "Australian Dollar", currencies, 198.0);
+	        assertEquals(221.76, convertedAmount, 0.01);
+	        convertedAmount =MainWindow.convert("Canadian Dollar", "Australian Dollar", currencies, 0.0);
+	        assertEquals(0.0, convertedAmount, 0.01);
+	        convertedAmount =MainWindow.convert("Canadian Dollar", "Australian Dollar", currencies, 1000000.0);
+	        assertEquals(1120000.0, convertedAmount, 0.01);
+	        convertedAmount =MainWindow.convert("Canadian Dollar", "Australian Dollar", currencies, 1000001.0);
+	        assertEquals(1120001.12, convertedAmount, 0.01);
+	        System.out.println(MainWindow.convert("Canadian Dollar", "Australian Dollar", currencies, -1.0));
 	        
 	    }
-
-	    @Test
-	    public void testNExiste() {
-	        Currency currency1 = new Currency("Livre Libanais", "LL");	  
-	        assertTrue(currency1.getExchangeValues().isEmpty());
-	    }
-	    
-	    
-	    @Test
-	    public void testDefaultValuesForCanadianDollar() {
-	        Currency canadianDollar = new Currency("Canadian Dollar", "CAD");
-	        canadianDollar.defaultValues();
-
-	        canadianDollar.setExchangeValues("USD", 0.74);
-	        canadianDollar.setExchangeValues("GBP", 0.58);
-	        canadianDollar.setExchangeValues("CHF", 0.64);
-	        canadianDollar.setExchangeValues("CNY", 5.26);
-	        canadianDollar.setExchangeValues("JPY", 108.26);
-	        canadianDollar.setExchangeValues("AUD", 1.12);
-	        canadianDollar.setExchangeValues("EUR", 0.68);
-	       
-	    }
-	    @Test
-	    public void testDefaultValuesForAustralianDollar() {
-	        Currency AustDollar = new Currency("Australian Dollar", "AUD");
-	        AustDollar.defaultValues();
-
-	        AustDollar.setExchangeValues("USD", 0.66);
-	        AustDollar.setExchangeValues("GBP", 0.52);
-	        AustDollar.setExchangeValues("CHF", 0.57);
-	        AustDollar.setExchangeValues("CNY", 4.65);
-	        AustDollar.setExchangeValues("JPY", 96.43);
-	        AustDollar.setExchangeValues("CAD", 0.89);
-
-	       
-	    }
-	    @Test
-	    public void testDefaultValuesForUSDDollar() {
-	        Currency UsdDollar = new Currency("US Dollar", "USD");
-	        UsdDollar.defaultValues();
-
-	        
-	        UsdDollar.setExchangeValues("CAN", 1.36);
-	        UsdDollar.setExchangeValues("AUD", 1.53);
-	        
-	       
-	    }
-	    @Test
-	    public void testDefaultValuesForBRitDollar() {
-	        Currency Brit = new Currency("British Pound", "GBP");
-	        Brit.defaultValues();
-
-	        Brit.setExchangeValues("CAN", 1.71);
-	        Brit.setExchangeValues("AUD", 1.92);
-	        
-	       
-	    }
-	    
-	    @Test
-	    public void testDefaultValuesForEURDollar() {
-	        Currency uro = new Currency("Euro", "EUR");
-	        uro.defaultValues();
-
-	        uro.setExchangeValues("CAN", 1.47);
-	        uro.setExchangeValues("AUD", 1.65);
-	        
-	       
-	    
-	    }
-
 	   void testAmount() {
 	        assertThrows(IllegalArgumentException.class, () -> {
 	        	

e9a5237711144b49a5210de8678431f6d83efc90
ds-e <dana.seif.eddine@gmail.com>
2023-12-07 17:58:46 -0500

    update boite blanche et noire

--- currency-converter-in-java-master/src/test/TestBoiteBlanche.java
+++ currency-converter-in-java-master/src/test/TestBoiteBlanche.java
@@ -1,85 +1,201 @@
 package test;
 
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.ArrayList;
 import org.junit.Test;
 import currencyConverter.Currency;
-import static org.junit.Assert.assertEquals;
-
-
 import currencyConverter.MainWindow;
 public class TestBoiteBlanche  {
 	  ArrayList<Currency> currencies = Currency.init();
-	 
+	 Double convertedAmount;
 	  
-	  public boolean PaysExiste(String pays){
-		   boolean existe=false;
-		 	for (int i=0;i>currencies.size();i++) {
-		 		if (currencies.get(i).getName()==pays) {
-		 			existe = true;	
-		 			return existe;
-		 			
-		 		}
-	   }
-		 	return existe;
-	  }  	
+		
+		public boolean deviseExiste(String devise) {
+		    for(Currency currency : currencies) {
+		        if (currency.getShortName().equals(devise)) {
+		            return true;
+		        }
+		    }
+		    return false;
+		}
+
+		@Test
+		public void checkDevisesExist() {
+	        String[] devisesSpe = {"USD", "CAD", "GBP", "EUR", "CHF", "AUD"};
+
+	        for(String d : devisesSpe) {
+	            try {
+	                assertTrue("Devise " + d + " n'existe pas", deviseExiste(d));
+	            } 
+	            catch (AssertionError e) {
+	                System.out.println(e.getMessage());
+	            }
+	        }
+	    }
+		
+		
+		@Test
+		public void incorrectCurrencySource_returns0() {
+			convertedAmount = MainWindow.convert("Lebanese Pound", "US Dollar", currencies, 500.0);
+//	        System.out.println("convertedAmount: " + convertedAmount);
+			assertEquals(0.0, convertedAmount);
+		}
+		@Test
+		public void incorrectCurrencyTarget_returns0() {
+			convertedAmount = MainWindow.convert("US Dollar", "United Arab Emirates Dirham", currencies, 500.0);
+			assertEquals(0.0, convertedAmount);
+		}
+		@Test
+		public void incorrectCurrencySourceAndTarget_returns0() {
+			convertedAmount = MainWindow.convert("Canadian Dollar", "Lebanese Pound", currencies, 500.0);
+			assertEquals(0.0, convertedAmount);
+		}
+
+	  
 	  @Test
 	 public void testMainConvert() {
-	 
-		  
-	       
-
-		 	//le dollar canadian existe, dollare australian existe
-		 	assertTrue(PaysExiste("Canadian Dollar"));
-		 	assertTrue(PaysExiste("Australian Dollar"));
-		 	//livre libanais n'existe pas
-	        assertFalse(PaysExiste("Livre Libanais"));
 
 	        //Couverture des i-chemins:
-	        Double convertedAmount =MainWindow.convert("Canadian Dollar","US Dollar", currencies,198.0);   
-	        assertEquals(146.52, convertedAmount, 0.01);
-	        convertedAmount = MainWindow.convert("Canadian Dollar","British Pound", currencies,198.0);
-	        assertEquals(114.84, convertedAmount, 0.01);
-	        convertedAmount = MainWindow.convert("Canadian Dollar", "Euro",currencies , 198.0);
-	        assertEquals(134.64, convertedAmount, 0.01);
-	        convertedAmount =  MainWindow.convert("Canadian Dollar", "Swiss Franc", currencies , 198.0);
-	        assertEquals(126.72, convertedAmount, 0.01);
-	        convertedAmount =MainWindow.convert("Canadian Dollar", "Australian Dollar", currencies, 198.0);
-	        assertEquals(221.76, convertedAmount, 0.01);
-	        convertedAmount =MainWindow.convert("Canadian Dollar", "Australian Dollar", currencies, 0.0);
-	        assertEquals(0.0, convertedAmount, 0.01);
-	        convertedAmount =MainWindow.convert("Canadian Dollar", "Australian Dollar", currencies, 1000000.0);
-	        assertEquals(1120000.0, convertedAmount, 0.01);
-	        convertedAmount =MainWindow.convert("Canadian Dollar", "Australian Dollar", currencies, 1000001.0);
-	        assertEquals(1120001.12, convertedAmount, 0.01);
+			try {
+		        convertedAmount =MainWindow.convert("Canadian Dollar","US Dollar", currencies,198.0);  
+		        System.out.println(convertedAmount);
+		        assertEquals("mauvaise conversion Canadian Dollar a US Dollar", 0.0, convertedAmount, 0.01);
+			}catch (AssertionError e) {
+                System.out.println(e.getMessage());
+            }
+			try {
+		        convertedAmount = MainWindow.convert("Canadian Dollar","British Pound", currencies,198.0);
+		        System.out.println(convertedAmount);
+		        assertEquals("mauvaise conversion Canadian Dollar a British Pound", 114.84, convertedAmount, 0.01);
+			}catch (AssertionError e) {
+                System.out.println(e.getMessage());
+            }
+			try {
+		        convertedAmount = MainWindow.convert("Canadian Dollar", "Euro",currencies , 198.0);
+		        System.out.println(convertedAmount);
+		        assertEquals("mauvaise conversion Canadian Dollar a Euro", 134.64, convertedAmount, 0.01);
+		  }catch (AssertionError e) {
+	          System.out.println(e.getMessage());
+	      }  
+			try {
+		        convertedAmount =  MainWindow.convert("Canadian Dollar", "Swiss Franc", currencies , 198.0);
+		        System.out.println(convertedAmount);
+		        assertEquals("mauvaise conversion Canadian Dollar a Swiss Franc", 126.72, convertedAmount, 0.01);
+			}catch (AssertionError e) {
+                System.out.println(e.getMessage());
+            }
+			try {
+		        convertedAmount =MainWindow.convert("Canadian Dollar", "Australian Dollar", currencies, 198.0);
+		        System.out.println(convertedAmount);
+		        assertEquals("mauvaise conversion Canadian Dollar a Australian Dollar", 221.76, convertedAmount, 0.01);
+			}catch (AssertionError e) {
+                System.out.println(e.getMessage());
+            }
+			try {
+		        convertedAmount =MainWindow.convert("Canadian Dollar", "Australian Dollar", currencies, 0.0);
+		        System.out.println(convertedAmount);
+		        assertEquals("mauvaise conversion Canadian Dollar a Euro", 0.0, convertedAmount, 0.01);
+			}catch (AssertionError e) {
+                System.out.println(e.getMessage());
+            }
+			try {
+		        convertedAmount =MainWindow.convert("Canadian Dollar", "Australian Dollar", currencies, 1000000.0);
+		        System.out.println(convertedAmount);
+		        assertEquals("mauvaise conversion Canadian Dollar a Euro", 1120000.0, convertedAmount, 0.01);
+			}catch (AssertionError e) {
+                System.out.println(e.getMessage());
+            }
+			try {
+		        convertedAmount =MainWindow.convert("Canadian Dollar", "Australian Dollar", currencies, 1000001.0);
+		        System.out.println(convertedAmount);
+		        assertEquals("mauvaise conversion Canadian Dollar a Euro", 1120001.12, convertedAmount, 0.01);
+			}catch (AssertionError e) {
+                System.out.println(e.getMessage());
+            }
+			try {
+		        //Couverture des arcs du graphe de flot de contrôle
+				Double mc = MainWindow.convert( currencies.get(4).getName(), currencies.get(1).getName(), currencies, 10.0);
+		        Double cc = Currency.convert( 10.0,currencies.get(4).getExchangeValues().get(currencies.get(1).getShortName()));
+				assertTrue("mc != cc", mc == cc);	        
+			}catch (AssertionError e) {
+                System.out.println(e.getMessage());
+            }
+			try {
+		        //test le couverture des instructions 
+				Double result = MainWindow.convert( currencies.get(1).getName(), currencies.get(2).getName(), currencies, -1.0);
+		        assertNull(result);
+			}catch (AssertionError e) {
+                System.out.println(e.getMessage());
+            }
+			try {
+		        assertNull(MainWindow.convert( currencies.get(1).getName(), currencies.get(2).getName(), currencies, 10000001.0));
+			}catch (AssertionError e) {
+                System.out.println(e.getMessage());
+            }
+			try {
+		        assertFalse(MainWindow.convert( currencies.get(1).getName(), currencies.get(2).getName(), currencies, 0.0)==0.0);
+			}catch (AssertionError e) {
+                System.out.println(e.getMessage());
+            }
+			try {
+		        assertNull("Amount 1000001 should return Null", Currency.convert(1000001.0, 1.0));
+			}catch (AssertionError e) {
+                System.out.println(e.getMessage());
+            }
+			try {
+		        assertNull("Amount -1 should return Null", Currency.convert(-1.0, 1.0));
+			}catch (AssertionError e) {
+                System.out.println(e.getMessage());
+            }
+			try {
+		        assertTrue("Amount 0 should return 0", Currency.convert(0.0, 1.0)==0.0);
+			}catch (AssertionError e) {
+                System.out.println(e.getMessage());
+            }
+			try {
+		        //	Couverture des chemins indépendants du graphe de flot de contrôle:
+		        assertNull(MainWindow.convert( currencies.get(4).getName(), currencies.get(1).getName(), currencies, -10.0));
+			}catch (AssertionError e) {
+                System.out.println(e.getMessage());
+            }
+			try { 
+		        assertNull(Currency.convert( -10.0,currencies.get(4).getExchangeValues().get(currencies.get(1).getShortName())));
+			}catch (AssertionError e) {
+                System.out.println(e.getMessage());
+            }
+			try {    
+		        assertNull(MainWindow.convert( currencies.get(4).getName(), currencies.get(1).getName(), currencies, 100001.0));
+			}catch (AssertionError e) {
+                System.out.println(e.getMessage());
+            }
+			try {  
+		        assertNull(Currency.convert( 100001.0,currencies.get(4).getExchangeValues().get(currencies.get(1).getShortName())));	        
+	
+			}catch (AssertionError e) {
+                System.out.println(e.getMessage());
+            }
+			try {
+		        
+		      //couverture des chemins indépendants 
+		        //si l'exchange value est null la valeur retourner doit etre vide et non zero
+		        assertNull(currencies.get(1).getExchangeValues().get(null));
+			}catch (AssertionError e) {
+                System.out.println(e.getMessage());
+            }
+			try {  
+		        //si curenci1 == curreci2 la valeur retourner doit etre vide
+		        assertNull(currencies.get(0).getExchangeValues().get("USA"));
+			}catch (AssertionError e) {
+                System.out.println(e.getMessage());
+            }
+			try {
+		        assertTrue(currencies.get(1).getExchangeValues().get("USA")==1.073);
 	        
-	        //Couverture des arcs du graphe de flot de contrôle
-	        assertTrue(MainWindow.convert( currencies.get(4).getName(), currencies.get(1).getName(), currencies, 10.0)==Currency.convert( 10.0,currencies.get(4).getExchangeValues().get(currencies.get(1).getShortName())));	        
-
-	        //test le couverture des instructions 
-	        assertNull(MainWindow.convert( currencies.get(1).getName(), currencies.get(2).getName(), currencies, -1.0));
-	        assertNull(MainWindow.convert( currencies.get(1).getName(), currencies.get(2).getName(), currencies, 10000001.0));
-	        assertFalse(MainWindow.convert( currencies.get(1).getName(), currencies.get(2).getName(), currencies, 0.0)==0.0);
-	        assertNull(Currency.convert(1000001.0, 1.0));
-	        assertNull(Currency.convert(-1.0, 1.0));
-	        assertTrue(Currency.convert(0.0, 1.0)==0.0);
-	        
-	        //	Couverture des chemins indépendants du graphe de flot de contrôle:
-	        assertNull(MainWindow.convert( currencies.get(4).getName(), currencies.get(1).getName(), currencies, -10.0));
-	        assertNull(Currency.convert( -10.0,currencies.get(4).getExchangeValues().get(currencies.get(1).getShortName())));
-	        assertNull(MainWindow.convert( currencies.get(4).getName(), currencies.get(1).getName(), currencies, 100001.0));
-	        assertNull(Currency.convert( 100001.0,currencies.get(4).getExchangeValues().get(currencies.get(1).getShortName())));	        
-
-	    
-	        
-	      //couverture des chemins indépendants 
-	        //si l'exchange value est null la valeur retourner doit etre vide et non zero
-	        assertNull(currencies.get(1).getExchangeValues().get(null));
-	        //si curenci1 == curreci2 la valeur retourner doit etre vide
-	        assertNull(currencies.get(0).getExchangeValues().get("USA"));
-	        assertTrue(currencies.get(1).getExchangeValues().get("USA")==1.073);
-	        
-	        
+			}catch (AssertionError e) {
+                System.out.println(e.getMessage());
+            }
 	        
 	 }
 
@@ -127,4 +243,3 @@ public class TestBoiteBlanche  {
 	    
 	    
 	    
-
--- currency-converter-in-java-master/src/test/TestBoiteNoire.java
+++ currency-converter-in-java-master/src/test/TestBoiteNoire.java
@@ -4,101 +4,102 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import currencyConverter.Currency;
 import currencyConverter.MainWindow;
 
-class MainWindowConvertTest {
+class TestBoiteNoire {
 	
     ArrayList<Currency> currencies = Currency.init();
-
-
+    Double result;
+    
 	@Test
 	void incorrectCurrencySource_returnsNull() {
-		System.out.println("----------in incorrectCurrencySource_returns0--------------");
-		Double result = MainWindow.convert("AED", "CAD", currencies, 500.0);
+		System.out.println("----------in incorrectCurrencySource_returnsNull--------------");
+		result = MainWindow.convert("AED", "CAD", currencies, 500.0);
         System.out.println("Result: " + result);
 		assertNull(result);
 	}
 	@Test
 	void incorrectCurrencyTarget_returnsNull() {
-		System.out.println("----------in incorrectCurrencyTarget_returns0--------------");
-		Double result = MainWindow.convert("USD", "JPY", currencies, 500.0);
+		System.out.println("----------in incorrectCurrencyTarget_returnsNull--------------");
+		result = MainWindow.convert("USD", "JPY", currencies, 500.0);
         System.out.println("Result: " + result);
 		assertNull(result);
 	}
 	@Test
 	void incorrectCurrencySourceAndTarget_returnsNull() {
-		System.out.println("----------in incorrectCurrencySourceAndTarget_returns0--------------");
-		Double result = MainWindow.convert("CNY", "KRW", currencies, 500.0);
+		System.out.println("----------in incorrectCurrencySourceAndTarget_returnsNull--------------");
+		result = MainWindow.convert("CNY", "KRW", currencies, 500.0);
         System.out.println("Result: " + result);
 		assertNull(result);
 	}
 	@Test  
 	void correctCurrencySourceAndTarget1_returnsNotNull() {
-		System.out.println("-------in correctCurrencySourceAndTarget1_returnsNot0-------");
-		Double result = MainWindow.convert("EUR", "GBP", currencies, 500.0);
+		System.out.println("-------in correctCurrencySourceAndTarget1_returnsNotNull-------");
+		result = MainWindow.convert("EUR", "GBP", currencies, 500.0);
         System.out.println("Result: " + result);
         assertNotNull(result);
 		
 	}
 	@Test 
 	void correctCurrencySourceAndTarget2_returnsNotNull() {
-		System.out.println("------in correctCurrencySourceAndTarget2_returnsNot0-------");
-		Double result = MainWindow.convert("CAD", "USD", currencies, 500.0);
+		System.out.println("------in correctCurrencySourceAndTarget2_returnsNotNull-------");
+		result = MainWindow.convert("CAD", "USD", currencies, 500.0);
         System.out.println("Result: " + result);
         assertNotNull(result);
 	}
 	@Test
 	void amountALotLessThan0_returnsNull() {
 		System.out.println("----------in amountALotLessThan0_returnsNull--------------");
-		Double result = MainWindow.convert("EUR", "GBP", currencies, -1500000.0);
+		result = MainWindow.convert("EUR", "GBP", currencies, -1500000.0);
         System.out.println("Result: " + result);
 		assertNull(result);
 	}
 	@Test
 	void amountLessThan0_returnsNull() {
-		System.out.println("----------in amountLessThan0_returns0--------------");
-		Double result = MainWindow.convert("EUR", "GBP", currencies, -1.0);
+		System.out.println("----------in amountLessThan0_returnsNull--------------");
+		result = MainWindow.convert("EUR", "GBP", currencies, -1.0);
         System.out.println("Result: " + result);
 		assertNull(result);
 	}
 	@Test
 	void amountEquals0_returns0() {
 		System.out.println("----------in amountEquals0_returns0--------------");
-		Double result = MainWindow.convert("EUR", "GBP", currencies, 0.0);
+		result = MainWindow.convert("EUR", "GBP", currencies, 0.0);
         System.out.println("Result: " + result);
 		assertEquals(0.0, result);
 	}
 	@Test
 	void amountInInterval_returnsNotNull() {
-		System.out.println("----------in amountInInterval_returnsNot0--------------");
-		Double result = MainWindow.convert("EUR", "GBP", currencies, 2500.0);
+		System.out.println("----------in amountInInterval_returnsNotNull--------------");
+		result = MainWindow.convert("EUR", "GBP", currencies, 2500.0);
         System.out.println("Result: " + result);
 		assertNotNull(result);
 	}
 	@Test
 	void amountIsMaxValue_returnsNotNull() {
-		System.out.println("----------in amountIsMaxValue_returnsNot0--------------");
-		Double result = MainWindow.convert("EUR", "GBP", currencies, 1000000.0);
+		System.out.println("----------in amountIsMaxValue_returnsNotNull--------------");
+		result = MainWindow.convert("EUR", "GBP", currencies, 1000000.0);
         System.out.println("Result: " + result);
 		assertNotNull(result);
 	}
 	@Test
 	void amountMoreThanMax_returnsNull() {
-		System.out.println("----------in amountMoreThanMax_returns0--------------");
-		Double result = MainWindow.convert("EUR", "GBP", currencies, 1000001.0);
+		System.out.println("----------in amountMoreThanMax_returnsNull--------------");
+		result = MainWindow.convert("EUR", "GBP", currencies, 1000001.0);
         System.out.println("Result: " + result);
         assertNull(result);
 	}
 	@Test
 	void amountALotMoreThanMax_returnsNull() {
-		System.out.println("----------in amountALotMoreThanMax_returns0--------------");
-		Double result = MainWindow.convert("Euro", "British Pound", currencies, 1500000.0);
+		System.out.println("----------in amountALotMoreThanMax_returnsNull--------------");
+		result = MainWindow.convert("Euro", "British Pound", currencies, 1500000.0);
         System.out.println("Result: " + result);
         assertNull(result);
 	}
+	
+	
+	
 }
-
